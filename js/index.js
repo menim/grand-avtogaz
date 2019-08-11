3dcb34ec5d3b4a42b0ddda188f227a95787d0e2b
menim <mel198629@gmail.com>
2019-08-11 13:55:12 +0300

    Add modal show hide feature

--- js/index.js
+++ js/index.js
@@ -1,17 +1,47 @@
-(function () {
-  let equipmentListBtns = Array.prototype.slice.call (
-    document.querySelectorAll ('.equipment__btn')
+(function() {
+  //show hide equipment description feature
+  let equipmentListBtns = Array.prototype.slice.call(
+    document.querySelectorAll('.equipment__btn')
   );
 
-  function toggleEquipment (e) {
-    this.classList.toggle ('equipment__btn--is-active');
-    console.log (this.parentNode.parentNode.nextElementSibling);
-    this.parentNode.parentNode.nextElementSibling.classList.toggle (
+  function toggleEquipment(e) {
+    this.classList.toggle('equipment__btn--is-active');
+    this.parentNode.parentNode.nextElementSibling.classList.toggle(
       'equipment__bottom--is-show'
     );
   }
 
-  equipmentListBtns.forEach (function (equipmentBtn) {
-    equipmentBtn.addEventListener ('click', toggleEquipment);
+  equipmentListBtns.forEach(function(equipmentBtn) {
+    equipmentBtn.addEventListener('click', toggleEquipment);
   });
-}) ();
+
+  //modal window feature
+  function openModal() {
+    var modalLink = document.querySelector('.download-link');
+    modalLink.addEventListener('click', function(event) {
+      // remove "#" from #modal
+      var target = this.getAttribute('href').substr(1);
+
+      // use dynamic target to reference given modal
+      var modalWindow = document.getElementById(target);
+
+      if (modalWindow.classList) {
+        modalWindow.classList.add('open');
+      }
+
+      event.preventDefault();
+    });
+  }
+
+  function closeModal() {
+    var modalOverlay = document.querySelector('.modal__overlay');
+    modalOverlay.addEventListener('click', function(event) {
+      // target the whole modal
+      var modalWindow = this.parentNode;
+      modalWindow.classList.remove('open');
+    });
+  }
+
+  openModal();
+  closeModal();
+})();
